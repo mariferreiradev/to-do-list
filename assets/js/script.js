e947a0f29a457a2078dd633d357cf81e387ce2a2
Mariana Ferreira <mariferreira.dev@gmail.com>
2022-11-08 14:11:18 -0300

    refactor add tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,39 @@ const entradaTarefa = document.querySelector('#input-tarefas')
 const btnAddTarefa = document.querySelector('.btn-add')
 const listTarefas = document.querySelector('.card-tarefas')
 
-function AddTarefas() {
+function addTarefas() {
+    const estadoCheckbox = false
+    const tarefa = entradaTarefa.value
+
+    addTarefaNaDom(estadoCheckbox, tarefa)
+    entradaTarefa.value = ''
+}
+
+function addTarefaNaDom(estadoCheckbox, tarefa) {
     let divTarefas = document.createElement('div')
 
     let checkBoxTarefas = document.createElement('input');
     checkBoxTarefas.setAttribute('type', 'checkbox');
+    if (estadoCheckbox) {
+        checkBoxTarefas.setAttribute('checked', true)
+    }
     divTarefas.append(checkBoxTarefas);
 
     let spanTarefas = document.createElement('span')
+    spanTarefas.innerText = tarefa
     divTarefas.append(spanTarefas)
 
-    spanTarefas.innerText = entradaTarefa.value
-    entradaTarefa.value = ''
-
-    let deletarTarefas = document.createElement('img')
-    deletarTarefas.setAttribute('src', './assets/img/icon-delete.png')
-    deletarTarefas.setAttribute('width', '20px')
-    divTarefas.append(deletarTarefas)
+    let imgLixeira = document.createElement('img')
+    imgLixeira.setAttribute('src', './assets/img/icon-delete.png')
+    imgLixeira.setAttribute('width', '20px')
+    imgLixeira.addEventListener('click', deletarTarefas)
+    divTarefas.append(imgLixeira)
 
     listTarefas.prepend(divTarefas)
 }
 
-btnAddTarefa.addEventListener('click', AddTarefas)
+function deletarTarefas (e) {
+    listTarefas.removeChild(e.target.parentElement)
+}
+
+btnAddTarefa.addEventListener('click', addTarefas)
